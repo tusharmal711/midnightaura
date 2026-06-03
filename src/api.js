9bd559659ef2207204dd696ce439b45a8074724c
tusharmal711 <tusharmal711@gmail.com>
2026-06-03 20:51:01 +0530

    changes done

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://midnightaura-1.onrender.com",
-  // baseURL: "http://localhost:8008",
+  baseURL: "http://localhost:8008",
   withCredentials: true, // cookie er jonno important
 });
